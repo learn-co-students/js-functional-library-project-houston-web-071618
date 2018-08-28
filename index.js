ad08b8c45639761177d89f620c3f87062fc57ddf
Ryhan Chowdhury <ryhanchowdhury@apples-MacBook-Pro-2.local>
2018-08-28 14:26:04 -0500

    uploading each function

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      //iterate through the collectionay and push it to a new collectionay.
+      if ( collection.constructor === Array) {
+        for (i = 0; i<collection.length; i++) {
+          callback(collection[i])
+        }
+      } else if (collection.constructor === Object) {
+        for (const key in collection) {
+          callback(collection[key])
+        }
+      }
+      return collection
     },
 
     map: function() {
@@ -15,7 +25,7 @@ fi = (function() {
     reduce: function() {
 
     },
-    
+
     functions: function() {
 
     },
